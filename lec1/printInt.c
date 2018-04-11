7ddef207dcc2a964fe34aaf36f9babf464651d1d
nandulee <nandulee@scorelab.org>
2018-04-11 22:44:13 +0530

    Add Lec1

---
+++ lec1/printInt.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+
+ int testInteger = 5;
+ printf("Number = %d \n", testInteger);
+ return 0;
+}
---
+++ lec1/readInt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+ float testInteger;
+	        
+ printf("Enter an integer: ");
+ scanf("%f",&testInteger);  
+
+ printf("Number = %.2f \n",testInteger);
+ return 0;
+}
