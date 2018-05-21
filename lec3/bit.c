8b600d5082e8a8a3bbe1e2626ca7c8caac3c65b1
nandulee <nandulee@scorelab.org>
2018-05-21 22:23:27 +0530

    Add lecture 3 resources

---
+++ lec3/bit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int printb(int);
+
+int main() {
+    unsigned int a = 12, b = 15, c=0;
+    c=a&b;
+    printf("Output %d & %d= %d\n",a,b,c);
+    printf("%d\n",printb(a));
+    printf("%d\n",printb(b));
+    printf("%d\n",printb(c));
+    return 0;
+}
+
+int printb(int n){
+ if (n == 0) return 0;
+ else return (n % 2) + 10 * printb(n / 2);
+}
---
+++ lec3/vowel.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    char c;
+    int isLowercaseVowel, isUppercaseVowel;
+
+    printf("Enter an alphabet: ");
+    scanf("%c",&c);
+
+    // evaluates to 1 (true) if c is a lowercase vowel
+    isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+    // evaluates to 1 (true) if c is an uppercase vowel
+
+    // evaluates to 1 (true) if either isLowercaseVowel or isUppercaseVowel is true
+    return 0;
+}
